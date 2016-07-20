8293fcd8dbb640e350a94dd6721f192bf8aa418f
moreMile <daydaygyj@gmail.com>
2016-07-20 11:07:25 +0800

    add vue

---
+++ vuex-demo/components/Cart.vue
@@ -0,0 +1,42 @@
+<template id="">
+  <div class="cart">
+    <h2>你的购物车</h2>
+    <p v-show="!products.length"><i>请选购商品</i></p>
+    <ul>
+      <li v-for="p in products">
+        {{ p.title }} - {{ p.price }} x {{ p.quantity }}
+      </li>
+    </ul>
+    <p>总计: {{ total }}</p>
+    <p>
+      <button
+        :disabled="!products.length"
+        @click="checkout(products)"
+        type="button">Checkout</button>
+    </p>
+    <p v-show="checkoutStatus">Checkout {{ checkoutStatus }}.</p>
+  </div>
+</template>
+<script>
+import { checkout } from '../vuex/actions'
+import { cartProducts } from '../vuex/getters'
+
+export default {
+  vuex: {
+    getters: {
+      products: cartProducts,
+      checkoutStatus: ({ cart }) => cart.lastCheckout
+    },
+    actions: {
+      checkout
+    }
+  },
+  computed: {
+    total () {
+      return this.products.reduce((total, p) => {
+        return total + p.price * p.quantity
+      }, 0)
+    }
+  }
+}
+</script>
---
+++ vuex-demo/components/ProductList.vue
@@ -0,0 +1,32 @@
+<template id="">
+  <ul>
+    <li v-for="p in products">
+      {{ p.title }} - {{ p.price }}
+      <br>
+      <button
+        type="button"
+        :disabled="!p.inventory"
+        @click="addToCart(p)">
+        Add to cart
+      </button>
+    </li>
+  </ul>
+</template>
+<script>
+import { getAllProducts, addToCart } from '../vuex/actions'
+
+export default {
+  vuex: {
+    getters: {
+      products: ({ products }) => products.all
+    },
+    actions: {
+      getAllProducts,
+      addToCart
+    }
+  },
+  created () {
+    this.getAllProducts()
+  }
+}
+</script>
